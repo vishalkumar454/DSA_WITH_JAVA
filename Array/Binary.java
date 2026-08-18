5f613696f83c145e3fbdec756ddabd7173225e28
vishalkumar <rajvishal71760@gmail.com>
2026-08-18 13:10:19 +0530

    binary search algorithm implemented

--- Array/Binary.java
+++ Array/Binary.java
@@ -2,31 +2,32 @@ package Array;
 
 class Binary {
 
-    public static int BinarySearch(int BinaryNum[], int key) {
+	public static int binarySearch(int[] arr, int element) {
 
-        int initalIndex = 0;
-        int lastIndex = BinaryNum.length -1;
-        
+		int initalIndex = 0;
+		int lastIndex = arr.length - 1;
 
-        while (initalIndex <= lastIndex) {
-            int mid = (initalIndex + lastIndex)/2;
+		for (int i = 0; i < arr.length; i++) {
+			int mid = (initalIndex + lastIndex) / 2;
 
-            if (BinaryNum[mid] == key) {
-                return mid;
-            }else if(BinaryNum[mid] > key){
-                lastIndex = mid - 1;
-            }else if(BinaryNum[mid] < key){
-                initalIndex = mid + 1;
-            }
-        }
-        return -1;
-    }
-    public static void main(String[] args) {
+			if (arr[mid] > element) {
+				lastIndex = mid - 1;
+			} else if (arr[mid] < element) {
+				initalIndex = mid + 1;
+			} else {
+				return mid;
+			}
+		}
 
-        int BinaryNum[] = {1,2,3,4,5,6,7,8,9,10,11,23};
-        int key = 0;
+		return -1;
+	}
 
-        System.out.println("the key present at index " + BinarySearch(BinaryNum, key));
-    }
-    
+	public static void main(String[] args) {
+
+		int BinaryNum[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 23 };
+		int key = -1;
+
+		System.out.println("the key present at index " + binarySearch(BinaryNum, key));
+	}
+
 }
